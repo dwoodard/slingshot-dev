d1f5002dd518f74dc11e569e9dea27090387d923
Dustin Woodard <dustin.woodard@gmail.com>
2021-06-10 20:49:01 -0600

    todo and LaradockInstall

--- packages/Dwoodard/Slingshot/src/Console/Commands/slingshot.php
+++ packages/Dwoodard/Slingshot/src/Console/Commands/slingshot.php
@@ -71,84 +71,87 @@ class slingshot extends Command
         $this->line(' ');
         switch ($slingshot) {
             case 'laradock':
-                $this->info('Installing Laradock');
-
-                $this->info('- Clone laradock');
-                if (!file_exists(base_path() . '/laradock')) {
-                    shell_exec("git clone https://github.com/Laradock/laradock.git");
-                    shell_exec("rm -rf laradock/.git");
-                    $this->info('   - removed laradock/.git');
-                    chdir(base_path() . '/laradock');
-                    copy('.env.example', '.env',);
-                } else {
-                    $this->info('   - laradock was already cloned');
-                }
-
-
-                //update .env for laradock
-                $laradockEnv = file_get_contents(base_path() . '/laradock/.env');
-                if (!str_contains($laradockEnv, 'DATA_PATH_HOST=../data')) {
-                    $laradockEnv = preg_replace("/DATA_PATH_HOST=.*/", "DATA_PATH_HOST=../data", $laradockEnv);
-                    $php_version = $this->anticipate('What version of php (8.0 - 7.4 - 7.3)',
-                        ['8.0', '7.4', '7.3'], '7.4'
-                    );
-                    $laradockEnv = preg_replace("/PHP_VERSION=.*/", "PHP_VERSION=" . $php_version, $laradockEnv);
-                    file_put_contents(base_path() . '/laradock/.env', $laradockEnv);
-                    $this->info('   - laradock .env was updated');
-                } else {
-                    $this->info('   - laradock .env was already updated');
-                }
-
-
-                //update .env for laravel
-                $filename = base_path() . '/.env';
-                $env = file_get_contents($filename);
-                if (!str_contains($env, 'DB_HOST=mysql')) {
-                    //Update envs
-                    $env = preg_replace("/DB_HOST=.*/", "DB_HOST=mysql", $env);
-                    $env = preg_replace("/REDIS_HOST=.*/", "REDIS_HOST=redis", $env);
-                    $env = preg_replace("/DB_USERNAME=.*/", "DB_USERNAME=root", $env);
-                    $env = preg_replace("/DB_PASSWORD=.*/", "DB_PASSWORD=root", $env);
-                    file_put_contents(base_path() . '/.env', $env);
-                    $this->info('   - laravel .env has been saved');
-
-                    //Add envs
-                    if (!str_contains($env, 'QUEUE_HOST=beanstalkd')) {
-                        file_put_contents($filename, 'QUEUE_HOST=beanstalkd' . "\n", FILE_APPEND | LOCK_EX);
-                    }
-
-                } else {
-                    $this->info('   - laravel .env was already updated');
-                }
-
-                //Add data directory
-                $filename = base_path() . '/data';
-                if (!file_exists($filename)) {
-                    mkdir($filename, 0777, true);
-                    $this->info('- data directory added');
-                } else {
-                    $this->info('- data directory exists');
-                }
-
-                //add .gitignore items
-                $this->info("- Check git " . base_path() . "/.gitignore");
-                $gitignore = file_get_contents(base_path() . '/.gitignore');
-                $gitignoreChecks = [
-                    '/data/',
-                    '!/laradock/.env',
-                ];
-                foreach ($gitignoreChecks as $check) {
-                    if (!str_contains($gitignore, $check)) {
-                        file_put_contents(base_path() . '/.gitignore', $check . "\n", FILE_APPEND | LOCK_EX);
-                    }
-                }
-
-
+                $this->LaradockInstall();
                 break;
             default:
                 $this->info("Installing $slingshot");
                 break;
         }
 
+    }
+
+    private function LaradockInstall(): void
+    {
+        $this->info('Installing Laradock');
+
+        $this->info('- Clone laradock');
+        if (!file_exists(base_path() . '/laradock')) {
+            shell_exec("git clone https://github.com/Laradock/laradock.git");
+            shell_exec("rm -rf laradock/.git");
+            $this->info('   - removed laradock/.git');
+            chdir(base_path() . '/laradock');
+            copy('.env.example', '.env',);
+        } else {
+            $this->info('   - laradock was already cloned');
+        }
+
+
+        //update .env for laradock
+        $laradockEnv = file_get_contents(base_path() . '/laradock/.env');
+        if (!str_contains($laradockEnv, 'DATA_PATH_HOST=../data')) {
+            $laradockEnv = preg_replace("/DATA_PATH_HOST=.*/", "DATA_PATH_HOST=../data", $laradockEnv);
+            $php_version = $this->anticipate('What version of php (8.0 - 7.4 - 7.3)',
+                ['8.0', '7.4', '7.3'], '7.4'
+            );
+            $laradockEnv = preg_replace("/PHP_VERSION=.*/", "PHP_VERSION=" . $php_version, $laradockEnv);
+            file_put_contents(base_path() . '/laradock/.env', $laradockEnv);
+            $this->info('   - laradock .env was updated');
+        } else {
+            $this->info('   - laradock .env was already updated');
+        }
+
+
+        //update .env for laravel
+        $filename = base_path() . '/.env';
+        $env = file_get_contents($filename);
+        if (!str_contains($env, 'DB_HOST=mysql')) {
+            //Update envs
+            $env = preg_replace("/DB_HOST=.*/", "DB_HOST=mysql", $env);
+            $env = preg_replace("/REDIS_HOST=.*/", "REDIS_HOST=redis", $env);
+            $env = preg_replace("/DB_USERNAME=.*/", "DB_USERNAME=root", $env);
+            $env = preg_replace("/DB_PASSWORD=.*/", "DB_PASSWORD=root", $env);
+            file_put_contents(base_path() . '/.env', $env);
+            $this->info('   - laravel .env has been saved');
+
+            //Add envs
+            if (!str_contains($env, 'QUEUE_HOST=beanstalkd')) {
+                file_put_contents($filename, 'QUEUE_HOST=beanstalkd' . "\n", FILE_APPEND | LOCK_EX);
+            }
+
+        } else {
+            $this->info('   - laravel .env was already updated');
+        }
+
+        //Add data directory
+        $filename = base_path() . '/data';
+        if (!file_exists($filename)) {
+            mkdir($filename, 0777, true);
+            $this->info('- data directory added');
+        } else {
+            $this->info('- data directory exists');
+        }
+
+        //add .gitignore items
+        $this->info("- Check git " . base_path() . "/.gitignore");
+        $gitignore = file_get_contents(base_path() . '/.gitignore');
+        $gitignoreChecks = [
+            '/data/',
+            '!/laradock/.env',
+        ];
+        foreach ($gitignoreChecks as $check) {
+            if (!str_contains($gitignore, $check)) {
+                file_put_contents(base_path() . '/.gitignore', $check . "\n", FILE_APPEND | LOCK_EX);
+            }
+        }
     }
 }
